f979e98d0e0d7a4ffc8668796da5c7c67ab3ffa1
Pippi <francesco.pippi@jaspit.com>
2024-02-08 17:20:32 +0100

    Add project files.

---
+++ CryptoString/AesEncryptDecrypt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class AesEncryptDecrypt
+{
+    private static readonly byte[] Key = Encoding.UTF8.GetBytes("ThisIsASecretKey123".PadRight(32, '\0')).Take(32).ToArray();
+    private static readonly byte[] IV = Encoding.UTF8.GetBytes("InitializationVector".PadRight(16, '\0')).Take(16).ToArray();
+
+    public static string Encrypt(string plainText)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                }
+                return Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+    }
+
+    public static string Decrypt(string cipherText)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CryptoString/Program.cs
@@ -0,0 +1,14 @@
+﻿
+public static class StartUp
+{
+    public static void Main()
+    {
+        string originalTest = "Prova Pippi ciao";
+
+        string encryptedText = AesEncryptDecrypt.Encrypt(originalTest);
+        Console.WriteLine($"Encrypted: {encryptedText}");
+
+        string decryptedText = AesEncryptDecrypt.Decrypt(encryptedText);
+        Console.WriteLine($"Decrypted: {decryptedText}");
+    }
+}
